128f996946499e1ef04d84999c54cf9be5ed1039
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-05-24 02:24:48 +0530

    Dark mode #132

---
+++ js/theme.js
@@ -0,0 +1,12 @@
+let body = document.getElementsByTagName("BODY")[0];
+let themeBtn = document.getElementById("themeImg");
+themeBtn.addEventListener('click',function(){
+    body.classList.toggle("darktheme");
+    if(body.classList.contains("darktheme")){
+        themeBtn.src ="assests/sun.png"
+    }
+    else{
+        themeBtn.src ="assests/moon.png"
+    }
+
+})
