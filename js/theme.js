7d916fb7f05ccc28ed37c26e6c5b23c9c5260113
Somesh Mallewar <someshmallewar7@gmail.com>
2023-06-14 14:39:53 +0530

    fixed

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,7 @@ let themeBtn = document.getElementById("themeImg");
 themeBtn.addEventListener('click',function(){
     body.classList.toggle("darktheme");
     if(body.classList.contains("darktheme")){
-        themeBtn.src ="assests/sun.png"
+        themeBtn.src ="assests/sunblack.png"
     }
     else{
         themeBtn.src ="assests/moon.png"
